ecnp-try-once/ns3-ecn-plus-tryonce

commit d930da21caa94508e9fab3054a35dc0fb8dba684

docs: make ./waf --doxygen-no-build actually work

wscript
@@ -1058,7 +1058,7 @@ def build(bld):
         bld.env['PRINT_BUILT_MODULES_AT_END'] = False 
 
     if Options.options.doxygen_no_build:
-        _doxygen(bld)
+        _doxygen(bld, skip_pid=True)
         raise SystemExit(0)
 
     if Options.options.docset_build:
@@ -1232,15 +1232,7 @@ class Ns3ShellContext(Context.Context):
         wutils.run_argv([shell], env, os_env)
 
 
-def _doxygen(bld):
-    env = wutils.bld.env
-    proc_env = wutils.get_proc_env()
-
-    if not env['DOXYGEN']:
-        Logs.error("waf configure did not detect doxygen in the system -> cannot build api docs.")
-        raise SystemExit(1)
-        return
-
+def _print_introspected_doxygen (bld):
     try:
         program_obj = wutils.find_program('print-introspected-doxygen', env)
     except ValueError: 
@@ -1267,6 +1259,18 @@ def _doxygen(bld):
     if subprocess.Popen([prog, '--output-text'], stdout=text_out, env=proc_env).wait():
         raise SystemExit(1)
     text_out.close()
+
+def _doxygen(bld, skip_pid=False):
+    env = wutils.bld.env
+    proc_env = wutils.get_proc_env()
+
+    if not env['DOXYGEN']:
+        Logs.error("waf configure did not detect doxygen in the system -> cannot build api docs.")
+        raise SystemExit(1)
+        return
+
+    if not skip_pid:
+        _print_introspected_doxygen(bld)
 
     _getVersion()
     doxygen_config = os.path.join('doc', 'doxygen.conf')

commit decdacb0f895cb2e5bbe754db17c1662607836da

docs: (fixes #163) run print-introspected-doxygen correctly
This fixes the wscript error introduced in commit d930da21

wscript
@@ -1229,6 +1229,8 @@ class Ns3ShellContext(Context.Context):
 
 
 def _print_introspected_doxygen(bld):
+    env = wutils.bld.env
+    proc_env = wutils.get_proc_env()
     try:
         program_obj = wutils.find_program('print-introspected-doxygen', env)
     except ValueError: 
@@ -1243,6 +1245,8 @@ def _print_introspected_doxygen(bld):
                    " You need to build ns-3 at least once before "
                    "generating doxygen docs...")
         raise SystemExit(1)
+
+    Logs.info("Running print-introspected-doxygen")
 
     # Create a header file with the introspected information.
     doxygen_out = open(os.path.join('doc', 'introspected-doxygen.h'), 'w')