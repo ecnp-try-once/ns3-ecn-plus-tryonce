lanante/ns-3-dev-11ax

wifi: Forward up MPDUs from an A-MPDU under reception as long as they arrive at the PHY

src/wifi/model/mac-low.cc
@@ -2599,9 +2599,14 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, RxSignalInfo rxSignalInf
   NS_LOG_FUNCTION (this << psdu << rxSignalInfo << txVector <<
                    statusPerMpdu.size () << std::all_of(statusPerMpdu.begin(), statusPerMpdu.end(), [](bool v) { return v; })); //returns true if all true
   bool normalAck = false;
-  bool ampduSubframe = false; //flag indicating the packet belongs to an A-MPDU and is not a VHT/HE single MPDU
-  if (txVector.IsAggregation ())
+  bool ampduSubframe = txVector.IsAggregation (); //flag indicating the packet belongs to an A-MPDU and is not a VHT/HE single MPDU
+  //statusPerMpdu is empty for intermediate MPDU forwarding.
+  //This function is called also once the PPDU has been fully received by the PHY,
+  //in that case statusPerMpdu carries the information about the received MPDUs.
+  if (ampduSubframe && !statusPerMpdu.empty ())
     {
+      //We are here if a S-MPDU is received or if all MPDUs of an A-MPDU have been
+      //received (but have been already forwarded up, so ReceiveOk won't be called)
       NS_ASSERT (psdu->IsAggregate ());
       ampduSubframe = true;
       auto n = psdu->begin ();
@@ -2609,9 +2614,6 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, RxSignalInfo rxSignalInf
       NS_ABORT_MSG_IF (psdu->GetNMpdus () != statusPerMpdu.size (), "Should have one receive status per MPDU");
 
       WifiMacHeader firsthdr = (*n)->GetHeader ();
-      NS_LOG_DEBUG ("duration/id=" << firsthdr.GetDuration ());
-      NotifyNav ((*n)->GetPacket (), firsthdr);
-
       if (firsthdr.GetAddr1 () == m_self)
         {
           //Iterate over all MPDUs and notify reception only if status OK
@@ -2644,7 +2646,10 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, RxSignalInfo rxSignalInf
                   else if (firsthdr.IsData () || firsthdr.IsQosData ())
                     {
                       NS_LOG_DEBUG ("Deaggregate packet from " << firsthdr.GetAddr2 () << " with sequence=" << firsthdr.GetSequenceNumber ());
-                      ReceiveOk ((*n), rxSignalInfo, txVector, ampduSubframe);
+                      if (psdu->IsSingle ())
+                        {
+                          ReceiveOk ((*n), rxSignalInfo, txVector, ampduSubframe);
+                        }
                       if (firsthdr.IsQosAck ())
                         {
                           NS_LOG_DEBUG ("Normal Ack");
@@ -2686,7 +2691,7 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, RxSignalInfo rxSignalInf
     }
   else
     {
-      /* Simple MPDU */
+      /* An MPDU has been received */
       NS_ASSERT (!psdu->IsAggregate ());
       ReceiveOk ((*psdu->begin ()), rxSignalInfo, txVector, ampduSubframe);
     }

src/wifi/model/wifi-phy-state-helper.cc
@@ -457,6 +457,17 @@ WifiPhyStateHelper::SwitchToChannelSwitching (Time switchingDuration)
 }
 
 void
+WifiPhyStateHelper::ContinueRxNextMpdu (Ptr<WifiPsdu> psdu, RxSignalInfo rxSignalInfo, WifiTxVector txVector)
+{
+  NS_LOG_FUNCTION (this << *psdu << rxSignalInfo << txVector);
+  std::vector<bool> statusPerMpdu;
+  if (!m_rxOkCallback.IsNull ())
+    {
+      m_rxOkCallback (psdu, rxSignalInfo, txVector, statusPerMpdu);
+    }
+}
+
+void
 WifiPhyStateHelper::SwitchFromRxEndOk (Ptr<WifiPsdu> psdu, RxSignalInfo rxSignalInfo, WifiTxVector txVector,
                                        uint16_t staId, std::vector<bool> statusPerMpdu)
 {
@@ -472,7 +483,6 @@ WifiPhyStateHelper::SwitchFromRxEndOk (Ptr<WifiPsdu> psdu, RxSignalInfo rxSignal
     {
       m_rxOkCallback (psdu, rxSignalInfo, txVector, statusPerMpdu);
     }
-
 }
 
 void

ecnp-try-once/ns3-ecn-plus-tryonce

src/wifi/model/mac-low.cc
@@ -2467,9 +2467,14 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, double rxSnr, WifiTxVect
 {
   NS_LOG_FUNCTION (this);
   bool normalAck = false;
-  bool ampduSubframe = false; //flag indicating the packet belongs to an A-MPDU and is not a VHT/HE single MPDU
-  if (txVector.IsAggregation ())
+  bool ampduSubframe = txVector.IsAggregation (); //flag indicating the packet belongs to an A-MPDU and is not a VHT/HE single MPDU
+  //statusPerMpdu is empty for intermediate MPDU forwarding.
+  //This function is called also once the PPDU has been fully received by the PHY,
+  //in that case statusPerMpdu carries the information about the received MPDUs.
+  if (ampduSubframe && !statusPerMpdu.empty ())
     {
+      //We are here if a S-MPDU is received or if all MPDUs of an A-MPDU have been
+      //received (but have been already forwarded up, so ReceiveOk won't be called)
       NS_ASSERT (psdu->IsAggregate ());
       ampduSubframe = true;
       auto n = psdu->begin ();
@@ -2477,9 +2482,6 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, double rxSnr, WifiTxVect
       NS_ABORT_MSG_IF (psdu->GetNMpdus () != statusPerMpdu.size (), "Should have one receive status per MPDU");
 
       WifiMacHeader firsthdr = (*n)->GetHeader ();
-      NS_LOG_DEBUG ("duration/id=" << firsthdr.GetDuration ());
-      NotifyNav ((*n)->GetPacket (), firsthdr);
-
       if (firsthdr.GetAddr1 () == m_self)
         {
           //Iterate over all MPDUs and notify reception only if status OK
@@ -2512,7 +2514,10 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, double rxSnr, WifiTxVect
                   else if (firsthdr.IsData () || firsthdr.IsQosData ())
                     {
                       NS_LOG_DEBUG ("Deaggregate packet from " << firsthdr.GetAddr2 () << " with sequence=" << firsthdr.GetSequenceNumber ());
-                      ReceiveOk ((*n), rxSnr, txVector, ampduSubframe);
+                      if (psdu->IsSingle ())
+                        {
+                          ReceiveOk ((*n), rxSnr, txVector, ampduSubframe);
+                        }
                       if (firsthdr.IsQosAck ())
                         {
                           NS_LOG_DEBUG ("Normal Ack");
@@ -2554,7 +2559,7 @@ MacLow::DeaggregateAmpduAndReceive (Ptr<WifiPsdu> psdu, double rxSnr, WifiTxVect
     }
   else
     {
-      /* Simple MPDU */
+      /* An MPDU has been received */
       NS_ASSERT (!psdu->IsAggregate ());
       ReceiveOk ((*psdu->begin ()), rxSnr, txVector, ampduSubframe);
     }

src/wifi/model/wifi-phy-state-helper.cc
@@ -462,6 +462,17 @@ WifiPhyStateHelper::SwitchToChannelSwitching (Time switchingDuration)
 }
 
 void
+WifiPhyStateHelper::ContinueRxNextMpdu (Ptr<WifiPsdu> psdu, double snr, WifiTxVector txVector)
+{
+  NS_LOG_FUNCTION (this << *psdu << snr << txVector);
+  std::vector<bool> statusPerMpdu;
+  if (!m_rxOkCallback.IsNull ())
+    {
+      m_rxOkCallback (psdu, snr, txVector, statusPerMpdu);
+    }
+}
+
+void
 WifiPhyStateHelper::SwitchFromRxEndOk (Ptr<WifiPsdu> psdu, double snr, WifiTxVector txVector, std::vector<bool> statusPerMpdu)
 {
   NS_LOG_FUNCTION (this << *psdu << snr << txVector << statusPerMpdu.size () <<
@@ -475,7 +486,6 @@ WifiPhyStateHelper::SwitchFromRxEndOk (Ptr<WifiPsdu> psdu, double snr, WifiTxVec
     {
       m_rxOkCallback (psdu, snr, txVector, statusPerMpdu);
     }
-
 }
 
 void