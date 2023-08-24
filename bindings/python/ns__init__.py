ecnp-try-once/ns3-ecn-plus-tryonce

commit a235c96b1d632cc0b888a38d1e46b86b65e115f8

python: Explicitly specify the encoding when opening files

bindings/python/ns__init__.py
@@ -155,7 +155,7 @@ def extract_linked_libraries(library_name: str, prefix: str) -> tuple:
     linked_libs = []
     # First discover which 3rd-party libraries are used by the current module
     try:
-        with open(os.path.abspath(library_path), "rb") as f:
+        with open(os.path.abspath(library_path), "rb", encoding="utf-8") as f:
             linked_libs = re.findall(b"\x00(lib.*?.%b)" % LIBRARY_EXTENSION.encode("utf-8"), f.read())
     except Exception as e:
         print("Failed to extract libraries used by {library} with exception:{exception}"

doc/contributing/pickle-to-xml.py
@@ -12,7 +12,7 @@ import os
 import codecs
 
 def dump_pickles(out, dirname, filename, path):
-    f = open(os.path.join(dirname, filename), 'r')
+    f = open(os.path.join(dirname, filename), 'r', encoding='utf-8')
     data = pickle.load(f)
     fragment_file = codecs.open(data['current_page_name'] + '.frag', mode='w', encoding='utf-8')
     fragment_file.write(data['body'])

doc/tutorial/pickle-to-xml.py
@@ -12,7 +12,7 @@ import os
 import codecs
 
 def dump_pickles(out, dirname, filename, path):
-    f = open(os.path.join(dirname, filename), 'r')
+    f = open(os.path.join(dirname, filename), 'r', encoding='utf-8')
     data = pickle.load(f)
     fragment_file = codecs.open(data['current_page_name'] + '.frag', mode='w', encoding='utf-8')
     fragment_file.write(data['body'])

src/flow-monitor/examples/flowmon-parse-results.py
@@ -179,7 +179,7 @@ class Simulation(object):
 
 
 def main(argv):
-    file_obj = open(argv[1])
+    file_obj = open(argv[1], encoding="utf-8")
     print("Reading XML file ", end=" ")
 
     sys.stdout.flush()

src/spectrum/utils/two-ray-to-three-gpp-ch-calibration.py
@@ -438,7 +438,7 @@ def print_cplusplus_map_from_fit_results(fit: pd.DataFrame, out_fname: str):
     out_str = out_str[0:-2]
     out_str += '}\n'
 
-    f = open(out_fname, "w")
+    f = open(out_fname, "w", encoding="utf-8")
     f.write(out_str)
     f.close()
 
@@ -499,7 +499,7 @@ if __name__ == '__main__':
             res = joblib.Parallel(n_jobs=10)(
                 joblib.delayed(fit_ftr_to_reference)(df, params_comb, num_search_grid_params, num_refinements) for params_comb in product(scenarios, is_los, frequencies))
 
-        f = open(fit_out_fname, "w")
+        f = open(fit_out_fname, "w", encoding="utf-8")
         f.write("scen\tcond\tfc\tsigma\tk\tdelta\tm\n")
         for line in res:
             f.write(line)

src/wifi/examples/reference/bianchi11ax.py
@@ -129,7 +129,7 @@ ack_rates_160MHz = [6e6, 12e6, 12e6, 24e6, 24e6, 24e6, 24e6, 24e6, 24e6, 24e6, 2
 k = 1
 
 difs = 1
-fo = open("bianchi_11ax_difs.txt", "w")
+fo = open("bianchi_11ax_difs.txt", "w", encoding="utf-8")
 for i in range(len(data_rates_20MHz)):
     bianchi_result = bianchi_ax(data_rates_20MHz[i], ack_rates_20MHz[i], k, difs)
     str_s = str_result(bianchi_result, i, 20)
@@ -149,7 +149,7 @@ for i in range(len(data_rates_160MHz)):
 fo.close()
 
 difs = 0
-fo = open("bianchi_11ax_eifs.txt", "w")
+fo = open("bianchi_11ax_eifs.txt", "w", encoding="utf-8")
 for i in range(len(data_rates_20MHz)):
     bianchi_result = bianchi_ax(data_rates_20MHz[i], ack_rates_20MHz[i], k, difs)
     str_s = str_result(bianchi_result, i, 20)

test.py
@@ -277,7 +277,7 @@ def translate_to_text(results_file, text_file):
     import xml.etree.ElementTree as ET
     et = ET.parse(results_file)
 
-    with open(text_file, 'w') as f:
+    with open(text_file, 'w', encoding='utf-8') as f:
         for test in et.findall('Test'):
             node_to_text(test, f)
 
@@ -303,7 +303,7 @@ def translate_to_html(results_file, html_file):
     html_file += '.html'
     print('Writing results to html file %s...' % html_file, end='')
 
-    with open(html_file, 'w') as f:
+    with open(html_file, 'w', encoding='utf-8') as f:
         f.write("<html>\n")
         f.write("<body>\n")
         f.write("<center><h1>ns-3 Test Results</h1></center>\n")
@@ -596,7 +596,7 @@ def read_ns3_config():
     f = None
     try:
         # sys.platform reports linux2 for python2 and linux for python3
-        f = open(lock_filename, "rt")
+        f = open(lock_filename, "rt", encoding="utf-8")
     except FileNotFoundError:
         print('The .lock-ns3 file was not found.  You must configure before running test.py.', file=sys.stderr)
         sys.exit(2)
@@ -616,7 +616,7 @@ def read_ns3_config():
     global NS3_BUILDDIR
     NS3_BUILDDIR = out_dir
 
-    with open(lock_filename) as f:
+    with open(lock_filename, encoding='utf-8') as f:
         for line in f.readlines():
             for item in interesting_config_items:
                 if line.startswith(item):
@@ -1296,7 +1296,7 @@ def run_tests():
     # do this since the tests will just append individual results to this file.
     #
     xml_results_file = os.path.join(testpy_output_dir, "results.xml")
-    with open(xml_results_file, 'w') as f:
+    with open(xml_results_file, 'w', encoding='utf-8') as f:
         f.write('<?xml version="1.0"?>\n')
         f.write('<Results>\n')
 
@@ -1768,7 +1768,7 @@ def run_tests():
             # XXX We could add some timing information to the examples, i.e. run
             # them through time and print the results here.
             #
-            with open(xml_results_file, 'a') as f:
+            with open(xml_results_file, 'a', encoding='utf-8') as f:
                 f.write('<Example>\n')
                 example_name = "  <Name>%s</Name>\n" % job.display_name
                 f.write(example_name)
@@ -1833,7 +1833,7 @@ def run_tests():
             # followed by a VALGR failing test suite of the same name.
             #
             if job.is_skip:
-                with open(xml_results_file, 'a') as f:
+                with open(xml_results_file, 'a', encoding='utf-8') as f:
                     f.write("<Test>\n")
                     f.write("  <Name>%s</Name>\n" % job.display_name)
                     f.write('  <Result>SKIP</Result>\n')
@@ -1841,10 +1841,10 @@ def run_tests():
                     f.write("</Test>\n")
             else:
                 if job.returncode == 0 or job.returncode == 1 or job.returncode == 2:
-                    with open(xml_results_file, 'a') as f_to, open(job.tmp_file_name) as f_from:
+                    with open(xml_results_file, 'a', encoding='utf-8') as f_to, open(job.tmp_file_name, encoding='utf-8') as f_from:
                         f_to.write(f_from.read())
                 else:
-                    with open(xml_results_file, 'a') as f:
+                    with open(xml_results_file, 'a', encoding='utf-8') as f:
                         f.write("<Test>\n")
                         f.write("  <Name>%s</Name>\n" % job.display_name)
                         f.write('  <Result>CRASH</Result>\n')
@@ -1864,7 +1864,7 @@ def run_tests():
     # individual pieces.  So, we need to finish off and close out the XML
     # document
     #
-    with open(xml_results_file, 'a') as f:
+    with open(xml_results_file, 'a', encoding='utf-8') as f:
         f.write('</Results>\n')
 
     #

utils.py
@@ -20,7 +20,7 @@ def get_list_from_file(file_path, list_name):
 
     # Read in the file if it exists.
     if os.path.exists(file_path):
-        with open(file_path, "r") as file_in:
+        with open(file_path, "r", encoding="utf-8") as file_in:
 
             # Look for the list.
             list_string = ""
@@ -58,7 +58,7 @@ def get_bool_from_file(file_path, bool_name, value_if_missing):
 
     # Read in the file if it exists.
     if os.path.exists(file_path):
-        with open(file_path, "r") as file_in:
+        with open(file_path, "r", encoding="utf-8") as file_in:
 
             # Look for the boolean variable.
             bool_found = False

commit 41681da2e296a8f9a00367fc1273c42e7e2a071b

bindings: (fixes #948) Rollback the file encoding changes made to ns__init__.py
Fixes issue introduced in a235c96b1d632cc0b888a38d1e46b86b65e115f8

bindings/python/ns__init__.py
@@ -155,7 +155,7 @@ def extract_linked_libraries(library_name: str, prefix: str) -> tuple:
     linked_libs = []
     # First discover which 3rd-party libraries are used by the current module
     try:
-        with open(os.path.abspath(library_path), "rb", encoding="utf-8") as f:
+        with open(os.path.abspath(library_path), "rb") as f:
             linked_libs = re.findall(b"\x00(lib.*?.%b)" % LIBRARY_EXTENSION.encode("utf-8"), f.read())
     except Exception as e:
         print("Failed to extract libraries used by {library} with exception:{exception}"